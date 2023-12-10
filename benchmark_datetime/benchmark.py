00cfd973c36d66c28d648867e3f790edbe757e37
Alessio <148966056+olk-m@users.noreply.github.com>
2023-12-10 20:40:12 +0200

    init

--- benchmark_datetime/benchmark.py
+++ benchmark_datetime/benchmark.py
@@ -5,9 +5,14 @@ from typing import Any
 
 import arrow
 import pendulum
+import pydantic
 import pytest
 import udatetime  # type: ignore[import-untyped]
 from dateutil import tz
+from faker import Faker
+from pydantic import TypeAdapter
+
+fake = Faker()
 
 libraries_now_utc = {
     "arrow": arrow.utcnow,
@@ -15,6 +20,7 @@ libraries_now_utc = {
     "pendulum": pendulum.now,
     "python": partial(datetime.datetime.now, datetime.UTC),
     "udatetime": udatetime.utcnow,
+    # "pydantic": ... # Not supported.
 }
 
 
@@ -29,9 +35,43 @@ libraries_now = {
     # "pendulum": ...,  # Not supported.
     "python": datetime.datetime.now,
     "udatetime": udatetime.now,
+    # "pydantic": ... # Not supported.
 }
 
 
 @pytest.mark.parametrize("library", libraries_now)
 def test_now(benchmark: Callable[..., Any], library: str) -> None:
-    benchmark(libraries_now_utc[library])
+    benchmark(libraries_now[library])
+
+
+libraries_parse_utc_from_unix_timestamp = {
+    "arrow": arrow.get,
+    # "dateutil": ...,  # Not supported.
+    "pendulum": pendulum.from_timestamp,
+    "python": datetime.datetime.utcfromtimestamp,
+    "udatetime": udatetime.utcfromtimestamp,
+    "pydantic": TypeAdapter[pydantic.AwareDatetime].validate_python,
+}
+
+
+@pytest.mark.parametrize("library", libraries_parse_utc_from_unix_timestamp)
+def test_parse_utc_from_timestamp(benchmark: Callable[..., Any], library: str) -> None:
+    benchmark(libraries_parse_utc_from_unix_timestamp[library], fake.unix_time())
+
+
+libraries_parse_utc_from_iso_8601 = {
+    "arrow": arrow.get,
+    # "dateutil": ...,  # Not supported.
+    "pendulum": pendulum.parse,  # type: ignore[attr-defined]
+    "python": datetime.datetime.fromisoformat,
+    "udatetime": udatetime.from_string,
+    "pydantic": TypeAdapter[pydantic.AwareDatetime].validate_python,
+}
+
+
+@pytest.mark.parametrize("library", libraries_parse_utc_from_iso_8601)
+def test_parse_utc_from_iso_8601(benchmark: Callable[..., Any], library: str) -> None:
+    benchmark(
+        libraries_parse_utc_from_iso_8601[library],
+        fake.iso8601(tzinfo=datetime.UTC),
+    )
